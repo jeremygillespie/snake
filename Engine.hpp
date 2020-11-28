6b961aff6d778a0133d251cb10913cc4b605189e
Jeremy Gillespie <jeremy@gillespiegames.com>
2020-11-28 18:33:41 -0500

    add Cost

--- Engine.hpp
+++ Engine.hpp
@@ -6,6 +6,33 @@
 
 namespace Snake {
 
+struct Cost {
+    float time;
+    float death;
+
+    bool operator==(const Cost &other) const {
+        return death == other.death && time == other.time;
+    }
+
+    bool operator<(const Cost &other) const {
+        return death == other.death ? time < other.time : death < other.death;
+    }
+
+    bool operator>(const Cost &other) const {
+        return death == other.death ? time > other.time : death > other.death;
+    }
+
+    bool operator<=(const Cost &other) const {
+        return death < other.death ||
+               (death == other.death && time <= other.time);
+    }
+
+    bool operator>=(const Cost &other) const {
+        return death > other.death ||
+               (death == other.death && time >= other.time);
+    }
+};
+
 class Engine {
 public:
     virtual AppleSearch::Path getPath(int nextApple) = 0;
@@ -13,14 +40,19 @@ public:
 
 class Exhaustive : public Engine {
 public:
-    Exhaustive(){
-        // TODO
-    };
+    Exhaustive(const State &start) : start{start} {}
 
     AppleSearch::Path getPath(int nextApple) {
         // TODO
         return AppleSearch::Path{};
     }
+
+private:
+    State start;
+
+    Cost evalPath(const State &s) { return Cost{}; }
+
+    Cost evalApple(const State &s) { return Cost{}; }
 };
 
 } // namespace Snake
