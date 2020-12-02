c137345e7136873d0ff21a96884f1573f5d878cc
Jeremy Gillespie <jeremy@gillespiegames.com>
2020-12-01 22:39:08 -0500

    rendering works

--- App.hpp
+++ App.hpp
@@ -25,13 +25,17 @@ private:
     bool running;
 
     SDL_Window *window;
-    SDL_Surface *windowSurface;
-    SDL_Surface *snakeSurface;
-    SDL_Surface *appleSurface;
-    SDL_Rect square;
+    SDL_Renderer *renderer;
+    SDL_Texture *snakeTexture;
+    SDL_Texture *appleTexture;
 };
 
-App::App() : running{true}, windowSurface{NULL} {}
+App::App() :
+        running{true},
+        window{NULL},
+        renderer{NULL},
+        snakeTexture{NULL},
+        appleTexture{NULL} {}
 
 int App::OnExecute() {
     if (OnInit() == false) {
@@ -58,25 +62,30 @@ bool App::OnInit() {
         return false;
     }
 
-    window =
-        SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                         SDL_WINDOWPOS_UNDEFINED, 900, 600, SDL_WINDOW_SHOWN);
-
+    window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, 900, 600,
+                              SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         return false;
     }
 
-    windowSurface = SDL_GetWindowSurface(window);
+    renderer = SDL_CreateRenderer(
+        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) {
+        return false;
+    }
 
-    snakeSurface = SDL_CreateRGBSurface(0, 32, 32, 32, 0, 0, 0, 0);
-    SDL_FillRect(snakeSurface, NULL,
-                 SDL_MapRGB(snakeSurface->format, 0, 255, 0));
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 
-    appleSurface = SDL_CreateRGBSurface(0, 32, 32, 32, 0, 0, 0, 0);
-    SDL_FillRect(appleSurface, NULL,
-                 SDL_MapRGB(snakeSurface->format, 255, 0, 0));
+    SDL_Surface *surface = SDL_CreateRGBSurface(0, 32, 32, 32, 0, 0, 0, 0);
 
-    square = SDL_Rect{0, 0, 32, 32};
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 200, 0));
+    snakeTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 200, 0, 0));
+    appleTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_FreeSurface(surface);
 
     return true;
 }
@@ -90,20 +99,30 @@ void App::OnEvent(SDL_Event *event) {
 void App::OnLoop() {}
 
 void App::OnRender() {
-    SDL_Rect snake{200, 200, 0, 0};
-    SDL_Rect apple{400, 400, 0, 0};
+    SDL_RenderClear(renderer);
 
-    SDL_BlitSurface(snakeSurface, &square, windowSurface, &snake);
-    SDL_BlitSurface(appleSurface, &square, windowSurface, &apple);
+    SDL_Rect src{0, 0, 1, 1};
 
-    SDL_UpdateWindowSurface(window);
+    SDL_Rect dst;
+    dst = {200, 20, 80, 80};
+    SDL_RenderCopy(renderer, snakeTexture, &src, &dst);
+    dst = {200, 120, 80, 80};
+    SDL_RenderCopy(renderer, snakeTexture, &src, &dst);
+    dst = {200, 220, 80, 80};
+    SDL_RenderCopy(renderer, appleTexture, &src, &dst);
+
+    SDL_RenderPresent(renderer);
 }
 
 void App::OnCleanup() {
-    SDL_FreeSurface(windowSurface);
-    windowSurface = NULL;
+    SDL_DestroyTexture(snakeTexture);
+    snakeTexture = NULL;
+    SDL_DestroyTexture(appleTexture);
+    appleTexture = NULL;
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    renderer = NULL;
     window = NULL;
 
     SDL_Quit();
