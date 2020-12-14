fd9684f4ad15a0d0e88c7074d746db3047e95ab1
Jeremy Gillespie <jeremy@gillespiegames.com>
2020-12-13 21:38:02 -0500

    init list

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,16 +1,29 @@
 #ifndef SNAKE_GRAPH_HPP
 #define SNAKE_GRAPH_HPP
 
+#include <vector>
+
 namespace snake {
 
 class graph {
 public:
-    graph();
+    int size_x, size_y, length;
 
-    int x, y, lenght;
+    std::vector<bool> occupied;
+    std::vector<bool> wall;
+
+    graph() : size_x{8}, size_y{8}, length{4}, occupied{}, wall{} {
+    }
+
+    void init(int x, int y) {
+        size_x = x;
+        size_y = y;
+        occupied.resize(size(), false);
+        wall.resize(size(), false);
+    }
 
     int size() {
-        return x * y;
+        return size_x * size_y;
     }
 };
 
